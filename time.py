c79063d5adf84a0ed9082edaa7c141166cca8ef7
Alexander Pivovarov <pivovaa@amazon.com>
2022-03-21 15:27:28 -0700

    Add p50 time and fps

--- time.py
+++ time.py
@@ -1,31 +1,50 @@
 import sys
 import tensorflow as tf
 import time
+import numpy as np
+from tensorflow.python.framework.convert_to_constants \
+                    import convert_variables_to_constants_v2
 
-physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-print("TF memory_growth:", tf.config.experimental.get_memory_growth(physical_devices[0]))
+# set_memory_growth(True) for GPU
+gpu_devices = tf.config.list_physical_devices('GPU')
+for gpu_dev in gpu_devices:
+    tf.config.experimental.set_memory_growth(gpu_dev, True)
 
-#MODEL = "centernet_resnet50_v2_512x512_coco17_tpu-8"
 MODEL = sys.argv[1]
 img_sz=int(sys.argv[2])
-dtype=tf.uint8
-batch=int(sys.argv[3]) if len(sys.argv) > 3 else 1
+#dtype=tf.uint8
+dtype=sys.argv[3]
+batch=int(sys.argv[4]) if len(sys.argv) > 4 else 1
 
 m=tf.saved_model.load(MODEL)
-ff=m.signatures['serving_default']
-x=tf.ones(shape=(batch,img_sz,img_sz,3), dtype=dtype)
-y = ff(x)
-y = ff(x)
-y = ff(x)
+f=m.signatures['serving_default']
+#ff=f
+ff = convert_variables_to_constants_v2(f) #, lower_control_flow=False)
+x2=tf.ones(shape=(batch,img_sz,img_sz,3), dtype=dtype)
+# Warmup
 N = 50
-t1 = time.time()
 for i in range(N):
+  x = tf.constant(x2)
   y = ff(x)
+
+N = 1000
+t1 = time.time()
+TTT=[]
+for i in range(N):
+  t10 = time.time()
+  x = tf.constant(x2)
+  y = ff(x)
+  t11 = time.time() - t10
+  TTT.append(t11)
 
 ttt = time.time() - t1
 print("-----------------------------------", MODEL, "--------------------")
-print("exec time:", ttt)
-print(batch*N/ttt, "fps")
-print("GPU Mem usage:", tf.config.experimental.get_memory_info('GPU:0'))
+print("exec time (sec):", ttt)
+print("AVG (fps)", batch*N/ttt)
+p50_time = np.percentile(TTT, 50) * 1000 / batch
+print("P50 (ms):", p50_time)
+print("P50 (fps):", 1000/p50_time)
+#print("GPU Mem usage:", tf.config.experimental.get_memory_info('GPU:0'))
 print("--------------------------------------------------------------------------")
+#print(TTT)
+print()
--- time.sh
+++ time.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
 MODELS=(
-"centernet_hg104_512x512_coco17_tpu-8;512"
-"centernet_resnet50_v2_512x512_coco17_tpu-8;512"
-"efficientdet_d0_coco17_tpu-32;512"
-"faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8;640"
-"faster_rcnn_resnet50_v1_640x640_coco17_tpu-8;640"
-"ssd_mobilenet_v2_320x320_coco17_tpu-8;320"
-"ssd_resnet50_v1_fpn_640x640_coco17_tpu-8;640"
-"mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8;1024"
+#"ssd_mobilenet_v2_320x320_coco17_tpu-8_float_batchN_nms/save_model;320;float32"
+#"ssd_mobilenet_v2_2-ml_g4dn;512;uint8"
+#"ssd_mobilenet_v2_2;320;uint8"
+"ssd_mobilenet_v2_2_trt_fp32;320;uint8"
+#"centernet_hourglass_512x512_1-ml_g4dn;512;uint8"
+#"centernet_resnet50v1_fpn_512x512_1-ml_g4dn;512;uint8"
+#"saved_model_trt_fp32;512;uint8"
+#"efficientdet_d0_1-ml_g4dn;512;uint8"
+#"faster_rcnn_resnet50_v1_640x640_1-ml_g4dn;640;uint8"
+#"faster_rcnn_resnet50_v1_640x640_1;640;uint8"
+#"faster_rcnn_inception_resnet_v2_640x640_1-ml_g4dn;640;uint8"
+#"mask_rcnn_inception_resnet_v2_1024x1024_1-ml_g4dn;512;uint8"
+#"mask_rcnn_inception_resnet_v2_1024x1024_1;512;uint8"
+#"resnet_50_classification_1-ml_g4dn;224;float32"
+#"aaa/compiled_models/1;640;uint8"
 )
 
+batch=1
 for M in ${MODELS[@]}
 do
   echo $M
-  IFS=";" read -r -a arr <<< "${M}"
-  echo ${arr[0]}
-  echo ${arr[1]}
+  LLL=$(echo $M | tr ";" "\n")
+  readarray -t arr <<< "$LLL"
+  #IFS=";" read -r -a arr <<< "${M}"
+  echo "model: ${arr[0]}"
+  echo "input size: ${arr[1]}"
+  echo "input dtype: ${arr[2]}"
+  echo "input batch: ${batch}"
 
-  python3.7 time.py "${arr[0]}/saved_model" ${arr[1]}
-  python3.7 time.py "${arr[0]}/saved_model_trt_fp32" ${arr[1]}
-  python3.7 time.py "${arr[0]}/saved_model_trt_fp16" ${arr[1]}
+  python3 time.py ${arr[0]} ${arr[1]} ${arr[2]} ${batch}
+  #python3.7 time.py "${arr[0]}/saved_model" ${arr[1]}
+  #python3.7 time.py "${arr[0]}/saved_model_trt_fp32" ${arr[1]}
+  #python3.7 time.py "${arr[0]}/saved_model_trt_fp16" ${arr[1]}
 done
